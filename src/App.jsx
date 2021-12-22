96d25dbc95f73b1b96d5c96f7c63abcdbe42e331
JonnathanE <jonnathan.espinoza@unl.edu.ec>
2021-12-22 13:32:55 -0500

    Setup Tailwind Configuration (Container, Custom Colors & Font )

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div>
+      <header className="container">
+        <h1 className='text-bookmark-purple'>Hola Mundo</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
